c381abb8815fce6d592e31a2cd92c07b74a93a66
RaymondCoder27 <mwebraymond13@gmail.com>
2025-02-07 14:55:03 +0300

    adds transaction id to admin agent

--- src/branchmanagerdomain/finances/stores/index.ts
+++ src/branchmanagerdomain/finances/stores/index.ts
@@ -374,7 +374,7 @@ export const useBilling = defineStore("billing", () => {
       }
 
       // Step 2: Create a new Float Ledger Entry with reduced amount
-      const { data } = await api.post(`/branch-manager/float-ledger`, {
+      const { data } = await api.post(`/branch-manager/add-float-ledger-record`, {
         requestId: floatRequest.id,
         date: new Date().toISOString(),
         description: floatRequest.description,
