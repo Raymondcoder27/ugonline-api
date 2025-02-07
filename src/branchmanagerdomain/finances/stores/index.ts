2d17bfe69f4d6cda8b9c85facea4fd923d5c88da
RaymondCoder27 <mwebraymond13@gmail.com>
2025-02-07 10:59:27 +0300

    adds transaction id to admin agent

--- src/branchmanagerdomain/finances/stores/index.ts
+++ src/branchmanagerdomain/finances/stores/index.ts
@@ -435,6 +435,30 @@ export const useBilling = defineStore("billing", () => {
     }
   }
 
+  //edit float request amount and allocated the new amount inserted in the form
+  async function editFloatRequest(requestId: any, payload: any) {
+    try {
+      const floatRequest = floatRequests.value.find((request) => request.id === requestId);
+      if (!floatRequest) {
+        console.error("Float request not found for ID:", requestId);
+        return;
+      }
+
+      const { data } = await api.put("/till-operator/edit-float-request/" + requestId, {
+        amount: payload.amount,
+        till: payload.till,
+        // status: "request edited",
+        status: "edited",
+        description: payload.description,
+        approvedBy: "Manager One",
+      });
+      floatRequests.value = data.data;
+      console.log("Float Requests:", floatRequests.value);
+    } catch (error) {
+      console.error("Error editing float request:", error);
+    }
+  }
+
 
 
   //approve the float request using the api
@@ -495,6 +519,7 @@ export const useBilling = defineStore("billing", () => {
     fetchFloatRequestsToAdmin,
     reduceFloatLedger,
     approveFloatRequest,
+    editFloatRequest,
     adjustFloatLedger,
     rejectFloatRequest,
     fetchFloatRequests,
